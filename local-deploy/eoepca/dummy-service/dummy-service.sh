e5c870831efe44aa219c622ffad54db3011ae6df
Richard Conway <richard@rconway.co.uk>
2021-11-09 16:31:00 +0000

    EOEPCA-483 fix-up protection for `dummy-service`

--- local-deploy/eoepca/dummy-service/dummy-service.sh
+++ local-deploy/eoepca/dummy-service/dummy-service.sh
@@ -13,16 +13,3 @@ ACTION="${@:-template}"
 
 # dummy-service
 helm -n test ${ACTION} --create-namespace --version 0.9.2 --values dummy-service-values.yaml dummy-service eoepca/dummy
-
-# protection
-#
-# client secret
-if [ "${ACTION}" != "template" ]; then
-  kubectl -n test create secret generic dummy-service-agent \
-    --from-file=../client.yaml \
-    --dry-run=client -o yaml \
-    | kubectl apply -f -
-fi
-#
-# resource-guard
-helm -n test ${ACTION} --create-namespace --version 0.0.52 --values dummy-service-guard-values.yaml dummy-service-guard eoepca/resource-guard
