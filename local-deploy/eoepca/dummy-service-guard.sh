704730d303ea92fa5ccf0bf98b01dc3de0119256
Richard Conway <richard@rconway.co.uk>
2022-05-09 14:18:54 +0000

    resource-guard@1.0.5 for the dummy-service

--- local-deploy/eoepca/dummy-service-guard.sh
+++ local-deploy/eoepca/dummy-service-guard.sh
@@ -106,5 +106,5 @@ else
   values | helm ${ACTION_HELM} dummy-service-guard resource-guard -f - \
     --repo https://eoepca.github.io/helm-charts \
     --namespace "${NAMESPACE}" --create-namespace \
-    --version 1.0.0
+    --version 1.0.5
 fi
