fbcf3679a77fd20d82dd52049b98e04f5012117a
Richard Conway <richard@rconway.co.uk>
2022-05-09 14:19:29 +0000

    Align pdp to eoepca v1.1

--- local-deploy/eoepca/pdp.sh
+++ local-deploy/eoepca/pdp.sh
@@ -36,5 +36,5 @@ else
   values | helm ${ACTION_HELM} pdp pdp-engine -f - \
     --repo https://eoepca.github.io/helm-charts \
     --namespace "${NAMESPACE}" --create-namespace \
-    --version 1.0.0
+    --version 1.1.2
 fi
