5646e3bba6da00e2158e9545ec995c5f17a60d63
Richard Conway <richard@rconway.co.uk>
2022-05-09 14:21:21 +0000

    Align user-profile to eoepca v1.1

--- local-deploy/eoepca/user-profile.sh
+++ local-deploy/eoepca/user-profile.sh
@@ -34,5 +34,5 @@ else
   values | helm ${ACTION_HELM} um-user-profile user-profile -f - \
     --repo https://eoepca.github.io/helm-charts \
     --namespace "${NAMESPACE}" --create-namespace \
-    --version 1.0.0
+    --version 1.1.2
 fi
