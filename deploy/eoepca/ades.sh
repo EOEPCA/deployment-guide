912baa5db7688eb69c34415e89461aac670b0c0a
Richard Conway <richard@rconway.co.uk>
2022-12-08 11:43:13 +0000

    Update to chart ades@2.0.2

--- deploy/eoepca/ades.sh
+++ deploy/eoepca/ades.sh
@@ -92,5 +92,5 @@ else
   values | helm ${ACTION_HELM} ades ades -f - \
     --repo https://eoepca.github.io/helm-charts \
     --namespace proc --create-namespace \
-    --version 2.0.1
+    --version 2.0.2
 fi
