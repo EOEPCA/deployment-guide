54cedd2427e68564885c223a935cab7f72e82c0a
Richard Conway <richard@rconway.co.uk>
2024-02-20 11:47:25 +0000

    Ignore errors for rm namespace already exists

--- deploy/cluster/minio.sh
+++ deploy/cluster/minio.sh
@@ -71,7 +71,7 @@ echo -e "\nMinio credentials..."
 if [ "${ACTION_HELM}" = "uninstall" ]; then
   kubectl -n "${NAMESPACE}" delete secret minio-auth
 else
-  kubectl create namespace "${NAMESPACE}"
+  kubectl create namespace "${NAMESPACE}" 2>/dev/null
   kubectl -n "${NAMESPACE}" create secret generic minio-auth \
     --from-literal=rootUser="${MINIO_ROOT_USER}" \
     --from-literal=rootPassword="${MINIO_ROOT_PASSWORD}" \
