1495c11d6f10901acc6b4c1ba819c5ab72cf686f
James Hinton <james.hinton@live.co.uk>
2024-09-11 15:59:58 +0000

    Update resource catalogue script for consistency

--- scripts/resource-catalogue/configure-resource-catalogue.sh
+++ scripts/resource-catalogue/configure-resource-catalogue.sh
@@ -1,45 +1,35 @@
 #!/bin/bash
 
 echo "Configuring the Resource Catalogue..."
+
+# Load utility functions
 source ../common/utils.sh
 
-# Retrieve values
+# Template paths
+template_path="./values-template.yaml"
+intermediate_output_path="./intermediate-values.yaml"
+final_output_path="./generated-values.yaml"
+
+# Generic user inputs
+ask INGRESS_HOST "Enter the base ingress host:" "example.com"
+ask CLUSTER_ISSUER "Specify the cert-manager cluster issuer for TLS certificates:" "letsencrypt-prod"
+ask DB_STORAGE_CLASS "Specify the Kubernetes storage class for database persistence:" "managed-nfs-storage-retain"
+
+# Collection user inputs
 ask RESOURCE_CATALOGUE_NAMESPACE "Enter the namespace for the Resource Catalogue:" "default"
 ask RESOURCE_CATALOGUE_INGRESS_ENABLED "Set ingress enabled to 'true' for direct access or 'false' if using identity gatekeeper:" "true"
-
-# if its disabled, then skip the ingress extra config.
-ask RESOURCE_CATALOGUE_INGRESS_HOST "Enter the public URL for the Resource Catalogue:" "resource-catalogue.example.com"
-ask INGRESS_CLASS "Specify the ingress class for the Resource Catalogue:" "nginx"
-
 ask RESOURCE_CATALOGUE_DB_ENABLED "Enable local database for the Resource Catalogue? (Set to 'false' if you are using an external DB)" "true"
-ask DB_STORAGE_CLASS "Specify the Kubernetes storage class for database persistence:" "managed-nfs-storage-retain"
-ask CLUSTER_ISSUER "Specify the cert-manager cluster issuer for TLS certificates:" "letsencrypt-prod"
 ask PYCSW_URL "Set the PyCSW server URL, typically the same as the ingress host:" "$INGRESS_HOST"
 
-# Generate values.yaml. 
-# Can live inside of this own repo. So move this out.
-cat >./generated-values.yaml <<EOF
-global:
-  namespace: ${RESOURCE_CATALOGUE_NAMESPACE}
-ingress:
-  enabled: ${RESOURCE_CATALOGUE_INGRESS_ENABLED}
-  name: resource-catalogue
-  host: ${RESOURCE_CATALOGUE_INGRESS_HOST}
-  tls_host: ${RESOURCE_CATALOGUE_INGRESS_HOST}
-  tls_secret_name: resource-catalogue-tls
-  class: ${INGRESS_CLASS}
-  className: ${INGRESS_CLASS}
-  annotations:
-    cert-manager.io/cluster-issuer: ${CLUSTER_ISSUER}
-db:
-  volume_storage_type: ${DB_STORAGE_CLASS}
-  config:
-    enabled: ${RESOURCE_CATALOGUE_DB_ENABLED}
-pycsw:
-  config:
-    server:
-      url: https://${PYCSW_URL}
-EOF
+# Apply replacements
+cp "$template_path" "$intermediate_output_path"
+replace_placeholder "$intermediate_output_path" "$final_output_path" "INGRESS_HOST" "$INGRESS_HOST"
+replace_placeholder "$final_output_path" "$final_output_path" "CLUSTER_ISSUER" "$CLUSTER_ISSUER"
+replace_placeholder "$final_output_path" "$final_output_path" "DB_STORAGE_CLASS" "$DB_STORAGE_CLASS"
+replace_placeholder "$final_output_path" "$final_output_path" "RESOURCE_CATALOGUE_NAMESPACE" "$RESOURCE_CATALOGUE_NAMESPACE"
+replace_placeholder "$final_output_path" "$final_output_path" "RESOURCE_CATALOGUE_INGRESS_ENABLED" "$RESOURCE_CATALOGUE_INGRESS_ENABLED"
+replace_placeholder "$final_output_path" "$final_output_path" "RESOURCE_CATALOGUE_DB_ENABLED" "$RESOURCE_CATALOGUE_DB_ENABLED"
+replace_placeholder "$final_output_path" "$final_output_path" "PYCSW_URL" "$PYCSW_URL"
+rm "$intermediate_output_path"
 
-echo "Configuration file generated: ./generated-values.yaml"
-echo "State file updated: $STATE_FILE"
+echo "Configuration file generated: $final_output_path"
