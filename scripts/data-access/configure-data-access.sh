a9522f5d8b7d3dee8e339c5e812bbcd9601ebea1
James Hinton <james.hinton@live.co.uk>
2024-10-09 14:18:28 +0000

    Add Data Access BB

--- scripts/data-access/configure-data-access.sh
+++ scripts/data-access/configure-data-access.sh
@@ -2,15 +2,20 @@
 
 # Load utility functions
 source ../common/utils.sh
-
-echo "Configuring the Data Access..."
+echo "Configuring the Data Access Building Block..."
 
 # Collect user inputs
-ask "CLUSTER_ISSUER" "Specify the cert-manager Cluster Issuer for TLS certificates (e.g., letsencrypt-prod)" "letsencrypt-prod" is_non_empty
-ask "INGRESS_HOST" "Enter the base domain for ingress hosts (e.g., example.com)" "example.com" is_valid_domain
-ask "DB_STORAGE_CLASS" "Specify the storage class for the database (e.g., managed-nfs-storage-retain)" "managed-nfs-storage-retain" is_non_empty
-ask "S3_ENDPOINT" "Enter the AWS S3 endpoint URL:" "minio.${INGRESS_HOST}"
+ask "INGRESS_HOST" "Enter the base ingress host" "example.com" is_valid_domain
+ask "CLUSTER_ISSUER" "Specify the cert-manager cluster issuer for TLS certificates" "letsencrypt-prod" is_non_empty
+ask "DB_STORAGE_CLASS" "Specify the Kubernetes storage class for database persistence" "managed-nfs-storage-retain" is_non_empty
 
-envsubst <"$TEMPLATE_PATH" >"$OUTPUT_PATH"
+ask "S3_HOST" "Enter the S3 Host URL (excluding https)" "minio.example.com" is_non_empty
+ask "S3_ACCESS_KEY" "Enter the S3 (MinIO) access key" "" is_non_empty
+ask "S3_SECRET_KEY" "Enter the S3 (MinIO) secret key" "" is_non_empty
 
-echo "✅ Configuration file generated: $OUTPUT_PATH"
+# Generate configuration files
+envsubst <"eoapi/values-template.yaml" >"eoapi/generated-values.yaml"
+envsubst <"stacture/values-template.yaml" >"stacture/generated-values.yaml"
+envsubst <"tyk-gateway/values-template.yaml" >"tyk-gateway/generated-values.yaml"
+envsubst <"tyk-gateway/redis-values-template.yaml" >"tyk-gateway/redis-generated-values.yaml"
+envsubst <"postgres/values-template.yaml" >"postgres/generated-values.yaml"
---
+++ scripts/data-access/validation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ../common/utils.sh
+source ../common/validation-utils.sh
+source "$HOME/.eoepca/state"
+
+# Check pods in data-access namespace
+check_pods_running "data-access" "" 5
+
+# Check services
+check_service_exists "data-access" "eoapi"
+check_service_exists "data-access" "stacture"
+check_service_exists "data-access" "tyk-gateway"
+
+# Check ingress
+check_url_status_code "https://eoapi.$INGRESS_HOST" "200"
+check_url_status_code "https://stacture.$INGRESS_HOST" "200"
+
+echo
+echo "All Resources in 'data-access' namespace:"
+echo
+kubectl get all -n data-access
