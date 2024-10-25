0286d96e899db57ffd1c0f1c27a7cf0863ac1d50
James Hinton <james.hinton@live.co.uk>
2024-10-25 14:58:03 +0000

    Update workspace validation

--- scripts/workspace/validation.sh
+++ scripts/workspace/validation.sh
@@ -4,17 +4,25 @@ source ../common/validation-utils.sh
 source "$HOME/.eoepca/state"
 
 # Check pods in workspace namespace
-check_pods_running "workspace" "" 5
+check_pods_running "workspace" "" 11
 
 # Check services
-check_service_exists "workspace" "workspace-api-v2"
+check_service_exists "workspace" "crossplane-webhooks"
+check_service_exists "workspace" "provider-minio"
+check_service_exists "workspace" "workspace-api"
+check_service_exists "workspace" "provider-kubernetes"
+check_service_exists "workspace" "function-patch-and-transform"
+check_service_exists "workspace" "workspace-admin-kong-manager"
+check_service_exists "workspace" "workspace-admin-kong-proxy"
+check_service_exists "workspace" "workspace-admin-api"
+check_service_exists "workspace" "workspace-admin-auth"
+check_service_exists "workspace" "workspace-admin-metrics-scraper"
+check_service_exists "workspace" "workspace-admin-web"
 check_service_exists "workspace" "workspace-ui"
-check_service_exists "workspace" "workspace-admin"
 
 # Check ingress
 check_url_status_code "$HTTP_SCHEME://workspace-api-v2.$INGRESS_HOST" "200"
 check_url_status_code "$HTTP_SCHEME://workspace-ui.$INGRESS_HOST" "200"
-check_url_status_code "$HTTP_SCHEME://workspace-admin.$INGRESS_HOST" "200"
 
 echo
 echo "All Resources in 'workspace' namespace:"
