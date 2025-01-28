5862d00a5dbbb648fe3eee99f1ed3854dfdf3382
James Hinton <james.hinton@live.co.uk>
2025-01-28 10:43:12 +0000

    Add ingress configuration and update resource catalogue scripts

--- scripts/resource-catalogue/configure-resource-catalogue.sh
+++ scripts/resource-catalogue/configure-resource-catalogue.sh
@@ -9,12 +9,7 @@ ask "INGRESS_HOST" "Enter the base domain name" "example.com" is_valid_domain
 ask "STORAGE_CLASS" "Specify the Kubernetes storage class for persistent volumes" "standard" is_non_empty
 configure_cert
 
-envsubst < "$TEMPLATE_PATH" >"$OUTPUT_PATH"
+envsubst <"$TEMPLATE_PATH" >"$OUTPUT_PATH"
+envsubst <"$INGRESS_TEMPLATE_PATH" >"$INGRESS_OUTPUT_PATH"
 
 echo "✅ Configuration file generated: $OUTPUT_PATH"
-
-if [ "$USE_CERT_MANAGER" == "no" ]; then
-    echo ""
-    echo "📄 Since you're not using cert-manager, please create the following TLS secrets manually before deploying:"
-    echo "- resource-catalogue-tls (for Resource Catalogue)"
-fi
