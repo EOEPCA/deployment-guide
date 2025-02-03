74a3c9f7d3cc2e595bd6f400b374b707371102bb
James Hinton <james.hinton@live.co.uk>
2025-02-03 14:33:53 +0000

    Add OAUTH2_USERNAME_KEY to app-hub values and update URLs in sharinghub and resource protection scripts

--- scripts/processing/oapip/resource-protection-validation.sh
+++ scripts/processing/oapip/resource-protection-validation.sh
@@ -2,14 +2,14 @@ source ./oapip-utils.sh
 
 echo ""
 echo "❌ Request sent without token:"
-curl "https://zoo.wo3.apx.develop.eoepca.org/ogc-api/processes"
+curl "https://zoo.${INGRESS_HOST}/ogc-api/processes"
 
 echo ""
 echo "✅ Request sent with token:"
 curl -H "Authorization: Bearer $ACCESS_TOKEN" \
-    "https://zoo.wo3.apx.develop.eoepca.org/ogc-api/processes"
+    "https://zoo.${INGRESS_HOST}/ogc-api/processes"
 
 echo ""
 echo "✅ Request sent with token:"
 curl -H "Authorization: Bearer $ACCESS_TOKEN" \
-    "https://zoo.wo3.apx.develop.eoepca.org/ogc-api/jobs"
+    "https://zoo.${INGRESS_HOST}/ogc-api/jobs"
