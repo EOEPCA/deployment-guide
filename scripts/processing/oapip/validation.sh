e6fc4dcc7c6e2bf522bff1a40a3edf0ef3c2ce9d
James Hinton <james.hinton@live.co.uk>
2025-02-10 09:19:17 +0000

    Add OIDC_OAPIP_ENABLED check for authentication in validation script

--- scripts/processing/oapip/validation.sh
+++ scripts/processing/oapip/validation.sh
@@ -1,8 +1,10 @@
-
-
 #!/bin/bash
 source ../../common/utils.sh
 source ../../common/validation-utils.sh
+
+if [ -z "$OIDC_OAPIP_ENABLED" ]; then
+    ask "OIDC_OAPIP_ENABLED" "Do you want to enable authentication using the IAM Building Block?" "true" is_boolean
+fi
 
 check_pods_running "processing" "app.kubernetes.io/name=zoo-project-dru-kubeproxy" 1
 check_pods_running "processing" "app.kubernetes.io/instance=zoo-project-dru" 3
@@ -17,8 +19,14 @@ check_service_exists "processing" "zoo-project-dru-service"
 check_service_exists "processing" "zoo-project-dru-postgresql"
 check_service_exists "processing" "zoo-project-dru-rabbitmq"
 
-check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST" "401"
-check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST/ogc-api/processes" "401"
+if [ "$OIDC_OAPIP_ENABLED" == "true" ]; then
+    check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST" "401"
+    check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST/ogc-api/processes" "401"
+else
+    check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST" "200"
+    check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST/ogc-api/processes" "200"
+fi
+
 check_url_status_code "$HTTP_SCHEME://zoo.$INGRESS_HOST/swagger-ui/oapip/" "200"
 
 echo
