6d35e29dba4729d6b8dd33e4f4d182e8c80bf554
Richard Conway <richard@rconway.co.uk>
2025-02-12 16:40:04 +0000

    Updates for application-quality BB

--- scripts/application-quality/validation.sh
+++ scripts/application-quality/validation.sh
@@ -3,10 +3,14 @@ source ../common/utils.sh
 source ../common/validation-utils.sh
 
 # Check if the Application Quality pod is running
-check_pods_running "application-quality" "app.kubernetes.io/name=application-quality" 1
+check_pods_running "application-quality" "app.kubernetes.io/component=api" 1
+check_pods_running "application-quality" "app=application-quality-db" 1
+check_pods_running "application-quality" "app.kubernetes.io/component=web" 1
 
 # Check service
-check_service_exists "application-quality" "application-quality"
+check_service_exists "application-quality" "application-quality-api"
+check_service_exists "application-quality" "application-quality-db"
+check_service_exists "application-quality" "application-quality-web"
 
 # Check ingress
 check_url_status_code "$HTTP_SCHEME://application-quality.${INGRESS_HOST}" "200"
