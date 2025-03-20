bc9244a5c619c632caaef09583b360d42aa15653
Richard Conway <richard.conway@telespazio.com>
2025-03-20 16:31:35 +0000

    [data-access] add STAC Manager UI (#93)

--- scripts/data-access/configure-data-access.sh
+++ scripts/data-access/configure-data-access.sh
@@ -17,6 +17,7 @@ ask "S3_SECRET_KEY" "Enter the S3 (MinIO) secret key" "" is_non_empty
 gomplate  -f "eoapi/$TEMPLATE_PATH" -o "eoapi/$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
 gomplate  -f "postgres/$TEMPLATE_PATH" -o "postgres/$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
 gomplate  -f "eoapi-support/$TEMPLATE_PATH" -o "eoapi-support/$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
+gomplate  -f "stac-manager/$TEMPLATE_PATH" -o "stac-manager/$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
 
 if [ "$INGRESS_CLASS" == "apisix" ]; then
     gomplate  -f "eoapi/$INGRESS_TEMPLATE_PATH" -o "eoapi/$INGRESS_OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
