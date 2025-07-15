e4b019e90582b576b83b9f9526d08c5312b5709f
Salvatore Pinto <xxx>
2025-07-15 08:45:45 +0000

    data access external db and object storage not required

--- scripts/data-access/configure-data-access.sh
+++ scripts/data-access/configure-data-access.sh
@@ -9,10 +9,14 @@ ask "INGRESS_HOST" "Enter the base domain name" "example.com" is_valid_domain
 ask "STORAGE_CLASS" "Specify the Kubernetes storage class for persistent volumes" "standard" is_non_empty
 configure_cert
 
-ask "S3_HOST" "Enter the S3 Host URL (excluding https)" "minio.${INGRESS_HOST}" is_non_empty
-ask "S3_ACCESS_KEY" "Enter the S3 (MinIO) access key" "" is_non_empty
-ask "S3_SECRET_KEY" "Enter the S3 (MinIO) secret key" "" is_non_empty
-
+ask "EXTERNAL_POSTGRES" "Do you want to use an eternal Postgres?" "false" is_non_empty
+if $EXTERNAL_POSTGRES; then
+  ask "EXTERNAL_POSTGRES_HOST" "External Postgres host" "eoapi-db.$INGRESS_HOST" is_non_empty
+  ask "EXTERNAL_POSTGRES_PORT" "External Postgres port" "5432" is_non_empty
+  ask "EXTERNAL_POSTGRES_DBNAME" "External Postgres database name" "eoapi" is_non_empty
+  ask "EXTERNAL_POSTGRES_USERNAME" "External Postgres database username" "eoapi" is_non_empty
+  ask "EXTERNAL_POSTGRES_PASSWORD" "External Postgres database password" "eoapi" is_non_empty
+fi
 
 gomplate  -f "eoapi/$TEMPLATE_PATH" -o "eoapi/$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
 gomplate  -f "postgres/$TEMPLATE_PATH" -o "postgres/$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
