11ce84c3b5a5a4c16f1629592ccb20afad9804b5
Richard Conway <richard.conway@telespazio.com>
2025-09-10 10:09:42 +0100

    Fix-up Resource Registration for Landsat harvester (#126)
    * [notebooks] Fix-up run script
    
    * [resource-registration] refine notebook
    
    * [resource-registration] fixup Landsat harvester
    
    * [notebooks] iam - clean outputs
    
    * [notebooks] resource-discovery - clean outputs
    
    * [notebooks] resource-registration - tweaks for harvester

--- notebooks/run.sh
+++ notebooks/run.sh
@@ -8,19 +8,34 @@ export PUSER="$(id -un)"
 export PUID="$(id -u)"
 export PGID="$(id -g)"
 
+export EXPOSE_PORT="${1:-8888}"
+
+COMPOSE_FILE="jupyterlab/docker-compose.yml"
+
 function onExit() {
-  docker-compose -f jupyterlab/docker-compose.yml down
+  if [ -n "${DOCKER_COMPOSE_CMD}" ]; then
+    ${DOCKER_COMPOSE_CMD} -f "${COMPOSE_FILE}" down
+  fi
   cd "${ORIG_DIR}"
 }
 trap "onExit" EXIT
 
-if ! hash docker 2>/dev/null; then
+# Check for docker support
+if ! command -v docker &>/dev/null; then
   echo "ERROR - docker is required" 1>&2
   exit 1
 fi
-if ! hash docker-compose 2>/dev/null && ! hash docker compose 2>/dev/null; then
+# Check for docker compose support
+if command -v docker-compose &>/dev/null; then
+  DOCKER_COMPOSE_CMD="docker-compose"
+fi
+if docker compose version &>/dev/null; then
+  DOCKER_COMPOSE_CMD="docker compose"
+fi
+if [ -z "${DOCKER_COMPOSE_CMD}" ]; then
   echo "ERROR - docker-compose or docker compose is required" 1>&2
   exit 1
 fi
 
-docker-compose -f jupyterlab/docker-compose.yml up --build
+xdg-open "http://localhost:${EXPOSE_PORT}"
+${DOCKER_COMPOSE_CMD} -f "${COMPOSE_FILE}" up --build
