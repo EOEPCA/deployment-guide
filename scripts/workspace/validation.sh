d0f110fce05dd81d6e725868e3401921d424076d
Richard Conway <richard@rconway.co.uk>
2025-11-13 17:16:48 +0000

    [workspace] refresh of deployment taking account of Crossplane as an infra component

--- scripts/workspace/validation.sh
+++ scripts/workspace/validation.sh
@@ -7,16 +7,20 @@ source "$HOME/.eoepca/state"
 check_pods_running "workspace" "" 11
 
 # Check services
-check_service_exists "workspace" "crossplane-webhooks"
-check_service_exists "workspace" "provider-minio"
-check_service_exists "workspace" "workspace-api"
-check_service_exists "workspace" "provider-kubernetes"
-check_service_exists "workspace" "function-patch-and-transform"
-check_service_exists "workspace" "workspace-admin-kong-proxy"
+check_service_exists "crossplane-system" "crossplane-webhooks"
+check_service_exists "crossplane-system" "provider-helm"
+check_service_exists "crossplane-system" "provider-keycloak"
+check_service_exists "crossplane-system" "provider-kubernetes"
+check_service_exists "crossplane-system" "provider-minio"
+check_service_exists "crossplane-system" "crossplane-contrib-function-auto-ready"
+check_service_exists "crossplane-system" "crossplane-contrib-function-environment-configs"
+check_service_exists "crossplane-system" "crossplane-contrib-function-python"
 check_service_exists "workspace" "workspace-admin-api"
 check_service_exists "workspace" "workspace-admin-auth"
+check_service_exists "workspace" "workspace-admin-kong-proxy"
 check_service_exists "workspace" "workspace-admin-metrics-scraper"
 check_service_exists "workspace" "workspace-admin-web"
+check_service_exists "workspace" "workspace-api"
 
 # Check ingress
 CHECK_URL_NO_REDIRECT=true check_url_status_code "$HTTP_SCHEME://workspace-api.$INGRESS_HOST/probe" "200"
