902c7f487d7b55858251e9fb730d19bbd22b9bbe
Richard Conway <richard@rconway.co.uk>
2025-12-05 16:15:48 +0000

    [resource-catalogue] support for ingress via nginx

--- scripts/resource-discovery/configure-resource-discovery.sh
+++ scripts/resource-discovery/configure-resource-discovery.sh
@@ -9,9 +9,15 @@ ask "INGRESS_HOST" "Enter the base domain name" "example.com" is_valid_domain
 ask "PERSISTENT_STORAGECLASS" "Specify the Kubernetes storage class for PERSISTENT data (ReadWriteOnce)" "local-path" is_non_empty
 configure_cert
 
+# Template - helm values
 gomplate  -f "$TEMPLATE_PATH" -o "$OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
-gomplate  -f "$INGRESS_TEMPLATE_PATH" -o "$INGRESS_OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
 
+# Template - ingress controller
+if [ "$INGRESS_CLASS" == "apisix" ]; then
+  gomplate  -f "$INGRESS_TEMPLATE_PATH" -o "$INGRESS_OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
+elif [ "$INGRESS_CLASS" == "nginx" ]; then
+  gomplate  -f "nginx-$INGRESS_TEMPLATE_PATH" -o "$INGRESS_OUTPUT_PATH" --datasource annotations="$GOMPLATE_DATASOURCE_ANNOTATIONS"
+fi
 
 echo "✅ Configuration file generated: $OUTPUT_PATH"
 echo "✅ Configuration file generated: $INGRESS_OUTPUT_PATH"
