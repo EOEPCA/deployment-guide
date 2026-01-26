8a8b973232a558af03943f49dc17395b10030e93
Richard Conway <richard@rconway.co.uk>
2026-01-26 14:54:04 +0000

    [registration] configurable eodata asset URL

--- scripts/resource-registration/check-prerequisites.sh
+++ scripts/resource-registration/check-prerequisites.sh
@@ -10,8 +10,11 @@ declare -a checks=(
     "check_helm_installed"
     "check_cert_manager_installed"
     "check_ingress_controller_installed"
-    "check_crossplane_installed"
     "check_rwx_storage"
 )
+
+if [[ "$RESOURCE_REGISTRATION_ENABLE_OIDC" == "yes" || "$RESOURCE_REGISTRATION_PROTECTED_TARGETS" == "yes" ]]; then
+    checks+=("check_crossplane_installed")
+fi
 
 run_validation "${checks[@]}"
